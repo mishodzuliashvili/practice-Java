fa79d2db434a0c71424e034ad87f3da9ca1e4e8c
mishodzuliashvili <mishodzuliashvili1@gmail.com>
2022-02-02 16:05:30 +0400

    Update index.java

--- index.java
+++ index.java
@@ -1,8 +1,12 @@
 //Demo commit
 class index{
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("Hey");
         System.out.println("Hello");
         System.out.println("Mr. M");
+=======
+        System.out.println("Hey bro");
+>>>>>>> parent of c5d160e (Changed)
     }
 }
