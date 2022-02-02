ee45d719156e47bd260b9f5a713443cf671f7845
mishodzuliashvili <mishodzuliashvili1@gmail.com>
2022-02-02 16:01:56 +0400

    Revert "newCommit"
    This reverts commit 641e74e1e2f2519582a8e4b5213fe15ea70b3206, reversing
    changes made to b89a5ffe8c1cb3b536f2b8feb17a4044c895a643.

--- index.java
+++ index.java
@@ -1,13 +1,8 @@
 //Demo commit
 class index{
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("Hey");
         System.out.println("Hello");
         System.out.println("Mr. M");
-=======
-        System.out.println("Hey bro");
-        System.out.println("Sup");
->>>>>>> c5d160e02d339c3feb182a30bd4bdb536ab42207
     }
 }
