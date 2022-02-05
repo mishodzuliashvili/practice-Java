9f33d676ae6288a938bf65592e1f6b727b01e88a
LukeGorgadze <just.luke.gorgadze@gmail.com>
2022-02-05 16:58:55 +0400

    Added random tester and toString method

--- SinglyLinkedList/SinglyLinkedList.java
+++ SinglyLinkedList/SinglyLinkedList.java
@@ -1,5 +1,7 @@
 package SinglyLinkedList;
 
+import java.util.Random;
+
 public class SinglyLinkedList<T> {
     private static class Node<T>{
         private T element;
@@ -56,9 +58,23 @@ public class SinglyLinkedList<T> {
         if(size == 0) last = null;
         return res;
     }
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        Node<T> temp = first;
+        String res = temp.getElement().toString() + ", ";
+        while (temp.getNext() != null) {
+            temp = temp.getNext();
+            if (temp.getNext() != null)
+                res += temp.getElement() + ", ";
+            else
+                res += temp.getElement();
+        }
+        return "SinglyLinkedList: " + "{ " + res + " }";
+    }
 
     public static void main(String[] args) {
-        var ll = new SinglyLinkedList<Integer>();
+       /* var ll = new SinglyLinkedList<Integer>();
         System.out.println(ll.getSize());
         System.out.println(ll.isEmpty());
         System.out.println(ll.removeFirst());
@@ -73,5 +89,33 @@ public class SinglyLinkedList<T> {
         System.out.println(ll.removeFirst());
         System.out.println(ll.last());
         System.out.println(ll.first());
+        */
+        //Random tester
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
+        Random r = new Random();
+        int randomTest = r.nextInt(0,100);
+        for(int i = 0; i < randomTest; i++)
+        {
+            int randomInt1 = r.nextInt(0,200);
+            list.addFirst(randomInt1);
+        }
+        System.out.println("List before modifying: \t");
+        System.out.println(list);
+        System.out.println("\n");
+        int randomInt2 = r.nextInt(0,200);
+        list.addFirst(randomInt2);
+        System.out.println("List after modifying: \t");
+        System.out.println(list);
+        System.out.println("\n");
+        int randomInt3 = r.nextInt(0,200);
+        System.out.println("Adding " + randomInt3 + " to the end \t");
+        list.addLast(randomInt3);
+        System.out.println(list);
+        System.out.println("\n");
+        System.out.println("Adding " + randomInt3 + " to the start \t");
+        list.addFirst(randomInt3);
+        System.out.println(list);
+
+
     }
 }
